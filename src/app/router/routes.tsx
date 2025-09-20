37eda7e010ca835fcaef4ffb88a3baf526e565f8
seanie3201 <seanie3201@gmail.com>
2025-09-20 14:04:48 +0900

    test1

--- src/app/router/routes.tsx
+++ src/app/router/routes.tsx
@@ -1,5 +1,5 @@
+import HomePage from '@/pages/home/ui/HomePage';
 import { createRootRoute, createRoute, createRouter } from '@tanstack/react-router';
-import { HomePage } from '@/pages';
 
 const rootRoute = createRootRoute();
 
